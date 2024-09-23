c578243f71112cd2968c29f1e99a388027a81c65
Ben Payton <bennyp2494@gmail.com>
2024-09-23 13:46:24 -0600

    Removed re as a dep because it is built in

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 import pathlib
 
 PROJECT_NAME = "ben_sci_tools"
-VERSION = "0.3.3"
+VERSION = "0.3.5"
 SHORT_DESCRIPTION = "useful python tools that I use to streamline my work."
 SOURCE_CODE_LINK= "https://github.com/Ben-Payton/ben_sci_tools"
 DOCUMENTATION_LINK = "https://github.com/Ben-Payton/ben_sci_tools/blob/main/README.md" 
-REQUIRED_DEPENDANCIES = ["numpy", "pandas", "matplotlib","re"]
+REQUIRED_DEPENDANCIES = ["numpy", "pandas", "matplotlib"]
 
 
 setuptools.setup(
